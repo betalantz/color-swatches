7d77bdc421b2aad8b3a14f8aed142cb2f85e3f35
Lantz Warrick <betalantz@gmail.com>
2019-03-14 16:21:04 -0700

    Updates

---
+++ precache-manifest.6f357f960f6baa54e4a26bfba3c5ad2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0f66c5b9da7c807f90a",
+    "url": "/color-swatches/static/css/main.015710a4.chunk.css"
+  },
+  {
+    "revision": "e0f66c5b9da7c807f90a",
+    "url": "/color-swatches/static/js/main.892aa727.chunk.js"
+  },
+  {
+    "revision": "6adb1b1cad21d75a5a73",
+    "url": "/color-swatches/static/js/runtime~main.27aa3f72.js"
+  },
+  {
+    "revision": "d6a64854756e1b04ac97",
+    "url": "/color-swatches/static/js/2.450b06c4.chunk.js"
+  },
+  {
+    "revision": "9b0c752424161139a2809410acdc859b",
+    "url": "/color-swatches/index.html"
+  }
+];
